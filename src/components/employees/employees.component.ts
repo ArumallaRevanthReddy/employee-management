55c347725a47a2dcd2fd2b57fe79d98803d5c459
revanthreddy128 <revanthreddy128@gmail.com>
2025-04-17 16:35:45 +0530

    innstalled mui and used table component

--- src/components/employees/employees.component.ts
+++ src/components/employees/employees.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+export interface EmployeeElement {
+  empId: number;
+  name: string;
+  jobTitle: string;
+  department: number;
+}
+
+const ELEMENT_DATA: EmployeeElement[] = [
+  { empId: 1, name: 'Hydrogen', jobTitle: 'Software Engineer', department: 1 },
+  { empId: 2, name: 'Helium', jobTitle: 'Software Engineer', department: 2 },
+  { empId: 3, name: 'Lithium', jobTitle: 'Software Engineer', department: 3 },
+  { empId: 4, name: 'Beryllium', jobTitle: 'Software Engineer', department: 4 },
+  { empId: 5, name: 'Boron', jobTitle: 'Software Engineer', department: 5 },
+];
 
 @Component({
   selector: 'app-employees',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
 export class EmployeesComponent {
-  private getEmployees() {}
+  displayedColumns: string[] = ['empId', 'name', 'jobTitle', 'department'];
+  dataSource = ELEMENT_DATA;
 
-  private getEmployeeById() {}
-
-  private editEmployee() {}
+  public openViewEmployee(row: any) {
+    console.log(row);
+  }
 }
