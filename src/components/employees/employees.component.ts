a85962f8413f4440db380a88211d3263da945a98
revanthreddy128 <revanthreddy128@gmail.com>
2025-05-06 15:43:51 +0530

    employee component

--- src/components/employees/employees.component.ts
+++ src/components/employees/employees.component.ts
@@ -1,32 +1,81 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { EmployeePopupComponent } from './employee-popup/employee-popup.component';
+import { EDepartment, EJobTitle } from '../../enums/common.enum';
+import { MatIconModule } from '@angular/material/icon';
 
 export interface EmployeeElement {
   empId: number;
   name: string;
-  jobTitle: string;
-  department: number;
+  jobTitle: EJobTitle;
+  department: EDepartment;
 }
 
 const ELEMENT_DATA: EmployeeElement[] = [
-  { empId: 1, name: 'Hydrogen', jobTitle: 'Software Engineer', department: 1 },
-  { empId: 2, name: 'Helium', jobTitle: 'Software Engineer', department: 2 },
-  { empId: 3, name: 'Lithium', jobTitle: 'Software Engineer', department: 3 },
-  { empId: 4, name: 'Beryllium', jobTitle: 'Software Engineer', department: 4 },
-  { empId: 5, name: 'Boron', jobTitle: 'Software Engineer', department: 5 },
+  {
+    empId: 1,
+    name: 'Hydrogen',
+    jobTitle: EJobTitle.SoftwareEngineer,
+    department: EDepartment.SoftwareDevelopment,
+  },
+  {
+    empId: 2,
+    name: 'Helium',
+    jobTitle: EJobTitle.SoftwareEngineer,
+    department: EDepartment.SoftwareDevelopment,
+  },
+  {
+    empId: 3,
+    name: 'Lithium',
+    jobTitle: EJobTitle.SoftwareEngineer,
+    department: EDepartment.SoftwareDevelopment,
+  },
+  {
+    empId: 4,
+    name: 'Beryllium',
+    jobTitle: EJobTitle.SoftwareEngineer,
+    department: EDepartment.SoftwareDevelopment,
+  },
+  {
+    empId: 5,
+    name: 'Boron',
+    jobTitle: EJobTitle.SoftwareEngineer,
+    department: EDepartment.SoftwareDevelopment,
+  },
 ];
 
 @Component({
   selector: 'app-employees',
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatIconModule, EmployeePopupComponent],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss',
 })
 export class EmployeesComponent {
-  displayedColumns: string[] = ['empId', 'name', 'jobTitle', 'department'];
-  dataSource = ELEMENT_DATA;
+  public displayedColumns: string[] = [
+    'empId',
+    'name',
+    'jobTitle',
+    'department',
+    'actions',
+  ];
+  public dataSource = ELEMENT_DATA;
+  public showPopup = false;
 
-  public openViewEmployee(row: any) {
-    console.log(row);
+  public selectedEmployee: EmployeeElement | undefined;
+
+  public openViewEmployeePopup(row: any) {
+    this.showPopup = true;
+    this.selectedEmployee = row;
   }
+
+  closePopup() {
+    this.showPopup = false;
+    this.selectedEmployee = undefined;
+  }
+
+  onEdit(row: EmployeeElement) {
+    this.openViewEmployeePopup(row);
+  }
+
+  onDelete(row: EmployeeElement) {}
 }
---
+++ src/enums/common.enum.ts
@@ -0,0 +1,15 @@
+export enum EJobTitle {
+  SoftwareEngineer = 'Software Engineer',
+  DataScientist = 'Data Scientist',
+  OperationsManager = 'Operations Manager',
+  ProjectManager = 'Project Manager',
+}
+
+export enum EDepartment {
+  Finance = 'Finance',
+  HumanResources = 'Human Resources',
+  Marketing = 'Marketing',
+  Operations = 'Operations',
+  Sales = 'Sales',
+  SoftwareDevelopment = 'Software Development',
+}
